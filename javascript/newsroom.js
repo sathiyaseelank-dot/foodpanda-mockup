e692ee704b7b045cf60a5fa9b51d8aa8a8d02e52
sathiyaseelank-dot <sathiyaseelank326@gmail.com>
2025-08-15 11:19:17 +0530

    First commit

---
+++ javascript/newsroom.js
@@ -0,0 +1,5 @@
+const click = (a) =>{
+    document.getElementsByClassName('search-box')[0].style.display=a;
+    document.getElementsByTagName('path')[0].setAttribute('style','transform: rotate(180deg) 5s');
+}
+
